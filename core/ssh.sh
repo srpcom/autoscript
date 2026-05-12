ef28f6b5d6a3ee7e65754dffa515b6a5591d1734
srpcom <182721422+srpcom@users.noreply.github.com>
2026-05-12 20:20:38 +0700

    Update ssh.sh

--- core/ssh.sh
+++ core/ssh.sh
@@ -56,7 +56,10 @@ add_trial_ssh() {
     echo "     CREATE TRIAL SSH ACCOUNT (60M)   "
     echo "======================================"
     
-    user="trialsrp-$(date +%m%d%H%M)"
+    # PERBAIKAN: Format Jam-Menit-Detik + 1 Karakter Random
+    rand_char=$(tr -dc 'a-z' < /dev/urandom | head -c 1)
+    user="trialsrp-$(date +%H%M%S)${rand_char}"
+    
     password="1"
     limit_ip=1
     masaaktif="60 Minutes"
