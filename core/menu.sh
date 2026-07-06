fb725a558af7fde098e65b9835d8a0811e56e6ec
srpcom <srpcom@users.noreply.github.com>
2026-07-06 10:49:28 +0700

    Add valid Cloudflare trusted_proxies to Caddyfile for stripping Cloudflare edge IPs

--- core/menu.sh
+++ core/menu.sh
@@ -214,6 +214,12 @@ rebuild_caddyfile() {
     local main_str="http://$main_domain, https://$main_domain"
     
     cat > /tmp/temp_caddyfile << EOF
+{
+    servers {
+        trusted_proxies static 173.245.48.0/20 103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 141.101.64.0/18 108.162.192.0/18 190.93.240.0/20 188.114.96.0/20 197.234.240.0/22 198.41.128.0/17 162.158.0.0/15 104.16.0.0/13 104.24.0.0/14 172.64.0.0/13 131.0.72.0/22
+    }
+}
+
 (proxy_rules) {
     handle /srpcom/* {
         reverse_proxy localhost:5000
